53108abe6f20d70a4230a7378f38012e88efc879
tkAcharya <tarunkumar.acharya4798@gmail.com>
2020-11-29 00:02:31 +0530

    fixed the before each error by using ganache.provider()

--- Inbox/compile.js
+++ Inbox/compile.js
@@ -2,6 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
+
 const inboxPath = path.resolve(__dirname,'contracts','Inbox.sol');
 const source = fs.readFileSync(inboxPath,'utf8');
 
--- Inbox/test/Inbox.test.js
+++ Inbox/test/Inbox.test.js
@@ -3,10 +3,26 @@ const ganache = require("ganache-cli");
 const Web3 = require("web3");
 // We used capital in Web3 since this will initialize using the constructor option
 //We require a provider to help us connect web3 to ganache
-const web3 = new Web3(ganache.provider);
+const web3 = new Web3(ganache.provider());
 
-describe("Dummy Test",() => {
-  it("First Test", () => {
-    assert.equal("Stopped","Stopped");
-  });
+
+
+// describe("Dummy Test",() => {
+//   it("First Test", () => {
+//     assert.equal("Stopped","Stopped");
+//   });
+// });
+
+beforeEach(() => {
+  //Get a list of all accounts
+  //Use one of them to deploy our contract
+  web3.eth.getAccounts()
+    .then(fetchedAccounts => {
+      console.log(fetchedAccounts);
+    })
+  //Every funtion in web3 is asynchronous so need to use .then
+});
+
+describe('Inbox Account Verifiation', () => {
+  it('deploys a contract',() => {});
 });
