8c8d02df013ef7ed8b454333226848b5fa6ddeca
tkAcharya <tarunkumar.acharya4798@gmail.com>
2020-11-29 00:25:22 +0530

    Compiled and deployed Inbox

--- Inbox/test/Inbox.test.js
+++ Inbox/test/Inbox.test.js
@@ -4,7 +4,7 @@ const Web3 = require("web3");
 // We used capital in Web3 since this will initialize using the constructor option
 //We require a provider to help us connect web3 to ganache
 const web3 = new Web3(ganache.provider());
-
+const {interface, bytecode} = require('../compile');
 
 
 // describe("Dummy Test",() => {
@@ -29,16 +29,24 @@ const web3 = new Web3(ganache.provider());
 //Creating a global var accounts
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
   //Get a list of all accounts
   //Use one of them to deploy our contract
   accounts = await web3.eth.getAccounts();
   //Every funtion in web3 is asynchronous so need to use .then
+
+  //Using accounts to deploy Inbox
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode , arguments: ['Hi there']})
+    .send({ from: accounts[0], gas: '1000000'})
 });
 
 describe('Inbox Account Verifiation', () => {
   it('deploys a contract',() => {
-    console.log(accounts);;
+    console.log(accounts);
+    console.log("Inbox:")
+    console.log(inbox);
   });
 });
